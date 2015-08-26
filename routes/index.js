de4f5290f86028d21bbbfb73d0b2071a39c096de
astigmatism <Astigmatism@Samus.local>
2015-08-25 22:20:41 -0700

    asd

--- routes/index.js
+++ routes/index.js
@@ -93,9 +93,8 @@ router.get('/google', function(req, res, next) {
                                                 console.log('resizing error: ' + err);
                                                 del(__dirname + '/../google/' + system + '/' + game, function (err, paths) {
                                                     console.log('Deleted files/folders:\n', paths.join('\n'));
+                                                    return nextgame(null);
                                                 });
-                                                fs.unlinkSync(delfile);
-                                                return nextgame(null);
                                             }
                                         });
                                     }
