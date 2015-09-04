0c35661ecce028ef3cd18d2400d088f2bf224753
astigmatism <Astigmatism@Samus.local>
2015-09-04 12:49:59 -0700

    new data

---
+++ config.js
@@ -0,0 +1,41 @@
+exports.data = {
+	systems: {
+		gb: {
+			name: "Nintendo Game Boy",
+			shortname: "gameboy",
+			romfileextentions: [
+				'gb',
+				'gbc'
+			]
+		},
+		gba: {
+			name: "Nintendo Game Boy Advance",
+			shortname: "GBA",
+			romfileextentions: [
+				'gba'
+			]
+		},
+		nes: {
+			name: "Nintendo Entertainment System (NES)",
+			shortname: "NES",
+			romfileextentions: [
+				'nes'
+			]
+		},
+		snes: {
+			name: "Super Nintendo (SNES)",
+			shortname: "SNES",
+			romfileextentions: [
+				'smc'
+			]
+		},
+		gen: {
+			name: "Sega Genesis",
+			shortname: "Genesis",
+			romfileextentions: [
+				'bin',
+				'32x'
+			]
+		}
+	}
+};
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ var async = require('async');
 var gm = require('gm');
 var del = require('del');
 var router = express.Router();
+var config = require('../config');
+var parseString = require('xml2js').parseString;
 
 router.get('/google', function(req, res, next) {
   	
@@ -166,6 +168,160 @@ router.get('/google', function(req, res, next) {
     				    console.log('next system');
     				    nextsystem(null);
     				});
+                });
+            });
+
+        }, function(err) {
+            if (err) {
+                return res.json(err);
+            }
+            res.json();
+        });
+    });
+});
+
+//stopped researching this because their search sucks against actual titles (it returned no results for "1942")
+router.get('/thegamesdb', function(req, res, next) {
+
+    var delay = req.query.delay || 10000;
+    var userip = req.query.userip || '192.168.1.2';
+
+    var systemnames = {
+        nes: 'nes',
+        snes: 'snes',
+        gen: 'genesis',
+        gb: 'gameboy',
+        gba: 'gba'
+    }
+
+    var i;
+
+    //open the data dir
+    fs.readdir(__dirname + '/../data', function(err, systems) {
+        if (err) {
+            return res.json(err);
+        }
+
+        //loop over each system
+        async.eachSeries(systems, function(system, nextsystem) {
+
+            //pass over "all" folder
+            if (system === 'all' || system.indexOf('.') === 0) {
+                return nextsystem(null);
+            }
+
+            console.log('beginning iteration over all ' + system + ' games.');
+
+            var systemfolder = __dirname + '/../thegamesdb/' + system;
+
+            fs.exists(systemfolder, function (exists) {
+                if (!exists) {
+                    fs.mkdirSync(systemfolder);
+                }
+
+                //read the genreated search.json file
+                fs.readFile(__dirname + '/../data/' + system + '/search.json', 'utf8', function(err, content) {
+
+                    try {
+                        content = JSON.parse(content);
+                    } catch (e) {
+                        return nextsystem(e);
+                    }
+
+                    var games = [];
+                    for (game in content) {
+                        if (content[game].r >= 87) {
+                            games.push(game);
+                        }
+                    }
+                    var ctr = 0;
+
+                    //loop over games
+                    async.eachSeries(games, function(game, nextgame) {
+
+                        ++ctr;
+                        var foldertocheck = __dirname + '/../thegamesdb/' + system + '/' + game;
+
+                        fs.exists(foldertocheck, function (exists) {
+                            if (exists) {
+                                return nextgame(null);
+                            } else {
+
+                                console.log('waiting to prevent spamming thegamesdb.... if you want to stop the application, do so now.');
+                                setTimeout(function() {
+
+                                    //build url
+                                    var term = encodeURIComponent(game);
+                                    var url = 'http://thegamesdb.net/api/GetGamesList.php?name=' + term + '&platform=' + config.data.systems[system].name;
+
+                                    console.log('thegamesdb: ' + system + ' ' + ctr + ': ' + game + ' --> ' + url);
+
+                                    request({
+                                        method: 'get',
+                                        url: url
+                                    }, function(err, response, body) {
+                                        if (err) {
+                                            return nextgame(response);
+                                        }
+                                        
+                                        console.log('search retunred');
+
+                                        parseString(body, function (err, result) {
+                                            if (err) {
+                                                return nextgame(err);
+                                            } 
+
+                                            if (result.Data.Game) {
+                                                console.log(JSON.stringify(result.Data.Game));
+                                            }
+                                            nextgame();
+                                        });
+
+                                        // body = JSON.parse(body);
+                                        
+                                        // if (body.responseData && body.responseData.results && body.responseData.results[0] && body.responseData.results[0].unescapedUrl) {
+
+                                        //     var imageurl = body.responseData.results[0].unescapedUrl; //the response must contain a url to the image
+
+                                        // } else {
+                                            
+                                        //     return nextgame(JSON.stringify(body)); //stop now if google suspects abuse
+                                        // }
+                                        
+                                        // console.log('create game folder');
+                                        // fs.mkdirSync(__dirname + '/../google/' + system + '/' + game);
+
+                                        // console.log('downloading');
+                                        // download(imageurl, __dirname + '/../google/' + system + '/' + game + '/original.jpg', function(filename){
+                                            
+                                        //     console.log('resizing asynconously..');
+
+                                        //     for (i = 0; i < resizes.length; ++i) {
+
+                                        //         gm(filename).resize(resizes[i]).write(__dirname + '/../google/' + system + '/' + game + '/' + resizes[i] + '.jpg', function (err) {
+                                        //             if (err) {
+                                        //                 console.log('resizing error: ' + err)
+                                        //             }
+                                        //         });
+                                        //     }
+
+                                        //     console.log('done!');
+                                        //     nextgame();
+                                            
+                                        //});
+                                    });
+
+                                }, delay);
+                            }
+                        });
+
+                    }, function(err) {
+                        if (err) {
+                            return nextsystem(err);
+                        }
+                        console.log('next system');
+                        nextsystem(null);
+                    });
                 });
             });
 
